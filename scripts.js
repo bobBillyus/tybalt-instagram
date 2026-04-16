7f262a97e1b0b1b8ee41d593a17b23253fe7439a
bobBillyus <aryand4120@gmail.com>
2026-04-15 21:40:42 -0400

    added pfp, stats, bio, grid, post modal to do: add posts + captions + comment feature + comments

--- scripts.js
+++ scripts.js
@@ -1,3 +1,18 @@
-// This JavaScript file can handle interactive features later
-console.log("Welcome to the Wiki!");
+const modal = document.getElementById("modal");
+const modalImg = document.getElementById("modalImg");
+const closeBtn = document.getElementById("closeBtn");
 
+function openModal(src) {
+    modal.style.display = "flex";
+    modalImg.src = src;
+}
+
+closeBtn.onclick = function () {
+    modal.style.display = "none";
+};
+
+modal.onclick = function (e) {
+    if (e.target === modal) {
+        modal.style.display = "none";
+    }
+};

f546ad701bcaa5e1c38d6de653d1b486a0386d70
bobBillyus <aryand4120@gmail.com>
2026-04-16 00:43:59 -0400

    Hashtag fix

--- scripts.js
+++ scripts.js
@@ -12,14 +12,11 @@ const captionBox = document.getElementById("modalCaption");
 
 let liked = false;
 
-/* OPEN MODAL */
 function openModal(img) {
     modal.style.display = "flex";
 
-    // image
     modalImg.src = img.src;
 
-    // caption + hashtags
     const captionText = img.dataset.caption || "";
     const hashtags = img.dataset.hashtags || "";
 
@@ -28,37 +25,31 @@ function openModal(img) {
         <p class="hashtags">${hashtags}</p>
     `;
 
-    // comments
     const commentsRaw = img.dataset.comments || "";
     commentList.innerHTML = commentsRaw
         ? commentsRaw.split("|").map(c => `<p>${c}</p>`).join("")
         : "<p>No comments yet</p>";
 
-    // reset like state
     liked = false;
     likeIcon.classList.remove("fa-solid");
     likeIcon.classList.add("fa-regular");
     likeIcon.style.color = "white";
 
-    // reset comment panel
     if (commentPanel) {
         commentPanel.style.display = "flex";
     }
 }
 
-/* CLOSE MODAL (button) */
 closeBtn.onclick = function () {
     modal.style.display = "none";
 };
 
-/* CLOSE MODAL (click outside) */
 modal.onclick = function (e) {
     if (e.target === modal) {
         modal.style.display = "none";
     }
 };
 
-/* LIKE TOGGLE */
 likeBtn.addEventListener("click", () => {
     liked = !liked;
 
